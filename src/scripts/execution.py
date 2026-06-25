786b2dbe77dfa69fba1e550500a9d2df03b5f589
Jordan Bignell <jordanbignell@hotmail.co.uk>
2026-06-25 22:58:11 +0100

    3rd commit for branch 002 - updating functions and strcuture

--- src/scripts/execution.py
+++ src/scripts/execution.py
@@ -1,7 +1,7 @@
-from abfs.config import service_client, read_adls_file_to_df
+from src.utils.config import service_client, read_adls_file
 
 file_system = service_client.get_file_system_client("jb-test-container")
 file_client = file_system.get_file_client("api_data/data.json")
 
-df = read_adls_file_to_df(file_client, "json")
+df = read_adls_file(file_client, file_name="data.json")
 print(df.head())
