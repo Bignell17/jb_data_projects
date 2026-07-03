33288993aed883aa748b511947a1374e16bdea9d
Jordan Bignell <Jordan.bignell@correla.com>
2026-07-03 11:51:28 +0100

    updating DPF

--- src/data_platform/data_engineering/main.py
+++ src/data_platform/data_engineering/main.py
@@ -1,7 +1,7 @@
 from framework.config_loader import load_config
 from framework.pipeline import Pipeline
 
-config = load_config("metadata/customer.yml")
+config = load_config("src/data_platform/data_engineering/metadata/abfs_folder_example.yaml")
 print(config)
 # pipeline = Pipeline(config)
 # pipeline.run()
